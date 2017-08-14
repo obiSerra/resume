adfaf41546a6bb83db10d8837a00c2c9d20caf22
Roberto Serra <roberto.serra@hoverstate.com>
2017-08-14 18:11:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const handlebars = require('handlebars'),
+      fs = require('fs'),
+      pdf = require('html-pdf'),
+      RSVP = require('rsvp');
+
+const basePath = 'file:///Users/robertoserra/dev/cv/templates/CV-Template/',
+      fileOut = 'output/Roberto_Serra_CV.pdf',
+      templateDir = 'templates/CV-Template/',
+      templateFile = 'index.html',
+      dataFile = 'info.json',
+      assets = ['headshot.jpg', 'style.css'];
+
+
+const htmlOutput = (html) => {
+    var fileName = 'output/index.html';
+    var stream = fs.createWriteStream(fileName);
+
+    stream.once('open', function(fd) {
+        stream.end(html);
+    });
+};
+
+const readData = new RSVP.Promise((resolve, reject) => {
+    fs.readFile(dataFile, 'utf8', function (error, data) {
+        if (error) reject(error);
+        resolve(JSON.parse(data));
+    });
+});
+
+readData
+    .then(data => {
+        console.log('[+] Reading template');
+        return new RSVP.Promise((resolve, reject) => {
+            fs.readFile(templateDir + templateFile, 'utf-8', function(error, source) {
+                if (error) reject(error);
+                const  template = handlebars.compile(source);
+                resolve(template(data));
+            });
+        });
+    })
+    .then(function(html) {
+        const options = { format: 'A4',
+                          base: basePath,
+                          timeout: 30000
+                        };
+        htmlOutput(html);
+
+
+        assets.forEach(ast => fs.createReadStream(templateDir + ast).pipe(fs.createWriteStream('output/' + ast)));
+
+        console.log('[+] Generating PDF');
+        return new RSVP.Promise((resolve, reject) => {
+            pdf.create(html, options).toFile(fileOut, (error, res) => {
+                if (error) reject(error);
+                resolve(res);
+            });
+        });
+    })
+    .then(res => {
+        console.log(`[+] Done ${res.filename}`);
+    })
+    .catch(error => console.log(error));
